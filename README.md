2055462cfdce9c1a59bbc7cca98b2da30f4c76cf
LucaSciacca <sciacca.luca93@gmail.com>
2018-02-23 15:28:55 +0100

    #first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# PHP csv builder #
+
+[![GitHub issues](https://img.shields.io/github/issues/Silverkron/php-csv-builder.svg)](https://github.com/Silverkron/php-csv-builder/issues)
+[![GitHub forks](https://img.shields.io/github/forks/Silverkron/php-csv-builder.svg)](https://github.com/Silverkron/php-csv-builder/network)
+[![GitHub license](https://img.shields.io/github/license/Silverkron/php-csv-builder.svg)](https://github.com/Silverkron/php-csv-builder/blob/master/LICENSE)
+[![Twitter](https://img.shields.io/twitter/url/https/github.com/Silverkron/php-csv-builder.svg?style=social)](https://twitter.com/intent/tweet?text=Wow:&url=https%3A%2F%2Fgithub.com%2FSilverkron%2Fphp-csv-builder)
+
+Build csv file and save or download it
---
+++ src/CsvBuilder.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: SilverKron
+ * Date: 23/02/18
+ */
+
+namespace CsvBuilder;
+
+class CsvBuilder
+{
+    private $filePath = '/tmp/';
+    private $fileName;
+    private $titles;
+    private $rows;
+
+    private $charForEscape = ' ';
+
+    /**
+     * CsvBuilder constructor.
+     * Set default values
+     */
+    public function __construct()
+    {
+        $this->fileName = 'csv_' . date('YmdHis') . '.csv';
+        $this->filePath = '/tmp/';
+    }
+
+    private function escape($param)
+    {
+        return str_replace(',', $this->charForEscape, $param);
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setFileName($name)
+    {
+        $this->fileName = $name;
+    }
+
+    /**
+     * @param string $path
+     */
+    public function setFilePath($path)
+    {
+        $path = rtrim($path, '/') . '/';
+        $this->filePath = $path;
+    }
+
+    /**
+     * @param array $titles
+     */
+    public function setTitles($titles)
+    {
+        $this->titles = array();
+        foreach ($titles as &$title) {
+            $title = $this->escape($title);
+        }
+
+        $this->titles = implode(',', $titles) . PHP_EOL;
+    }
+
+    public function clearRows()
+    {
+        $this->rows = array();
+    }
+
+    /**
+     * @param array $row
+     */
+    public function addRow($row)
+    {
+        foreach ($row as &$column) {
+            $column = $this->escape($column);
+        }
+
+        $this->rows .= implode(',', $row) . PHP_EOL;
+    }
+
+    /**
+     * @param string $char
+     * @throws \Exception
+     */
+    public function setCharForEscape($char)
+    {
+        if ($char == ',') {
+            throw new \Exception('You can not use comma for global escape');
+        }
+
+        $this->charForEscape = $char;
+    }
+
+    public function create()
+    {
+        $path = $this->filePath . $this->fileName;
+
+        $file = fopen($path, 'w');
+        fwrite($file, $this->titles);
+        fwrite($file, $this->rows);
+        fclose($file);
+
+        return $path;
+    }
+
+    public function download()
+    {
+        $path = $this->filePath . $this->fileName;
+
+        if (file_exists($path)) {
+            header('Content-Type: application/octet-stream');
+            header('Content-Transfer-Encoding: Binary');
+            header('Content-Disposition: attachment; filename=' . basename($path));
+            header('Content-Length: ' . filesize($path));
+            @readfile($path);
+            return true;
+        }
+        return false;
+    }
+}
